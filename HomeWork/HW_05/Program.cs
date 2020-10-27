b9cddc6528a7d222ec83e5c2dcc7e4c951be4ab5
ElovaMor <Snapeius@yandex.ru>
2020-10-27 17:00:35 +1000

    Solved 1-6 tasks from Lesson_1

---
+++ HomeWork/HW_05/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Студент: Баранова Виталина Васильевна
+Задача 5
+а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+б) Сделать задание, только вывод организуйте в центре экрана
+в) *Сделать задание б с использованием собственных методов (например, Print(string ms, int x, int y)
+*/
+
+using System;
+
+namespace HW_05
+{
+	class Program
+	{
+		static void PrintCenter(string msg)
+		{
+			Console.SetCursorPosition((Console.WindowWidth - msg.Length) / 2, Console.WindowHeight / 2);
+			Console.WriteLine(msg);
+		}
+		static void Main(string[] args)
+		{
+			string msg = "Баранова Виталина Васильевна Комсомольск-на-Амуре";
+
+			PrintCenter(msg);
+
+			Console.ReadLine();
+
+		}
+	}
+}
